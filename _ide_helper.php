14f23c7ad3a19ddd5e152d6d2f878f494bdfaabd
dcynsd <dcynsd@gmail.com>
2018-12-01 09:09:28 +0800

    ide-helper-model

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.44 on 2018-12-01 09:00:45.
+ * Generated for Laravel 5.5.44 on 2018-12-01 09:06:11.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
